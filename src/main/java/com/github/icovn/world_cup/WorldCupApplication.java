5e9f591664515e0f410df5b51dc290ca530d38c8
Nguyễn Quang Huy <quanghuy.ico@gmail.com>
2022-12-03 18:05:19 +0700

    feat: enable scheduling

--- src/main/java/com/github/icovn/world_cup/WorldCupApplication.java
+++ src/main/java/com/github/icovn/world_cup/WorldCupApplication.java
@@ -19,8 +19,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
+@EnableScheduling
 @Slf4j
 @SpringBootApplication
 public class WorldCupApplication implements CommandLineRunner {
